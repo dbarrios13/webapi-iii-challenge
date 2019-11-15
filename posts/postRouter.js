98c07e622906a7f3baf6e5a8e21ca2284d27b4d2
dbarrios13 <barriosadavid@gmail.com>
2019-11-14 20:28:42 -0700

    added users and posts routers

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,35 @@
-const express = 'express';
+const express = require('express')
 
-const router = express.Router();
+const postDb = require('./postDb')
 
-router.get('/', (req, res) => {
+const router = express.Router()
 
+router.get('/', async (req, res) => {
+    try {
+        const posts = await postDb.get()
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error retrieving the posts'
+        })
+    }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId, (req, res) => {
+    postDb.getById(req.params.id)
+        .then(res => {
+            res.status()
+        })
 
+    
+    // try {
+    //     const post = await postDb.getById(req.params.id)
+    //     res.status(200).json(post)
+    // } catch (error) {
+    //     res.status(500).json({
+    //         message: 'Error retrieving the post'
+    //     })
+    // }
 });
 
 router.delete('/:id', (req, res) => {
@@ -21,7 +43,13 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
-
+    if(req.params.id) {
+        next()
+    } else {
+        res.status(404).json({
+            message: 'The post with the specific ID does not exist'
+        })
+    }
 };
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-const express = 'express';
+const express = require('express')
+
+const posts = require('./posts/postRouter')
+
+const users = require('./users/userRouter')
+
+const helmet = require('helmet')
 
 const server = express();
+
+server.use(express.json())
+server.use(helmet())
+server.use(logger)
+
+server.use('/posts', posts)
+
+server.use('/users', users)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +23,9 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  let now = Date()
+  console.log(`A ${req.method} request to '${req.url}' on ${now}`)
+  next()
 };
 
 module.exports = server;
